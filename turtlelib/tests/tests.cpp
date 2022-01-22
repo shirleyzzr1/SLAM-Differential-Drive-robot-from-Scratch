f9878424549f0b24de0453092c9fe836c926f73e
shirleyzzr1 <zhuoruzhang2021@u.northwestern.edu>
2022-01-21 18:53:05 -0600

    add more test cases

--- turtlelib/tests/tests.cpp
+++ turtlelib/tests/tests.cpp
@@ -3,23 +3,6 @@
 #include "turtlelib/rigid2d.hpp"
 using namespace turtlelib;
 
-Vector2D v = {0,1};
-Transform2D tf;
-Transform2D tf1(v,90);
-
-// TEST_CASE( "inverse", "[transform]" ) {
-//     REQUIRE(tf1.rotation()==Approx(90));
-//     REQUIRE(tf1.translation().x == 0);
-//     REQUIRE(tf1.translation().y ==1);
-//     std::ostringstream out;
-//     Vector2D v2 = {1,2};
-//     out << v2;
-//     std::istringstream in;
-//     Vector2D v3;
-//     in >> v3;
-//     REQUIRE( "[1 2]"==out.str() );
-// }
-
 /// \brief test all the vectors related function
 TEST_CASE("vector test","[vectors]"){
     std::ostringstream out;
@@ -65,10 +48,32 @@ TEST_CASE("transform test","[transform2D]"){
 
     Transform2D tf5;
     tf5 = tf4.inv();
+    REQUIRE(tf5.rotation()==Approx(-1.57).margin(1e-1));
+    REQUIRE(tf5.translation().x ==-1);
+    REQUIRE(tf5.translation().y == Approx(0).margin(1e-3) );
+
+    Transform2D tf6;
+    tf6 = tf4*tf5;
+    std::ostringstream out3;
+    out3 << tf6;
+    REQUIRE( "deg: 0 x: 0 y: 0\n"==out3.str() );
+
+    tf4*=tf5;
+    std::ostringstream out4;
+    out4 << tf4;
+    REQUIRE( "deg: 0 x: 0 y: 0\n"==out4.str() );
     
 }
 
 ///\brief test all the transformation on matrix and vector
 TEST_CASE("transform and vectors","[]"){
-    
+    Vector2D v1 = {1,3};
+    std::istringstream in("0 1 1");
+    Transform2D tf1;
+    in >> tf1;
+    Vector2D v2 = tf1(v1);
+
+    REQUIRE(v2.x==2);
+    REQUIRE(v2.y == 4);
+
 }
