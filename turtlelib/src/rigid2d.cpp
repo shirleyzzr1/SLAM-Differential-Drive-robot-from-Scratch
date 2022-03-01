39b4047eca0c4118af3f2c4ac2fde3aa89303e81
shirleyzzr1 <zhuoruzhang2021@u.northwestern.edu>
2022-02-28 21:23:00 -0600

    modify normalize angle

--- turtlelib/src/rigid2d.cpp
+++ turtlelib/src/rigid2d.cpp
@@ -6,11 +6,23 @@
 namespace turtlelib{
 
     double normalize_angle(double rad){
-        if ((rad>=-PI) & (rad<=PI)) return rad;
-        int factor = floor(rad/PI);
-        if (factor<0)factor+=1;
-        rad  = rad-factor*PI;
-        return rad;
+        // if ((rad>=-PI) & (rad<=PI)) return rad;
+        // int factor = floor(rad/PI);
+        // if (factor<0)factor+=1;
+        // rad  = rad-factor*PI;
+        // return rad;
+        // reduce the angle  
+        int angle;
+        angle = int(rad2deg(rad));
+        angle =  angle % 360; 
+
+        // force it to be the positive remainder, so that 0 <= angle < 360  
+        angle = (angle + 360) % 360;  
+
+        // force into the minimum absolute value residue class, so that -180 < angle <= 180  
+        if (angle > 180)  
+            angle -= 360; 
+        return deg2rad(angle);
     }
 
     std::ostream & operator<<(std::ostream & os, const Vector2D & v){
